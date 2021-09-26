727397ca7413d2c0255a0ef20242f20072157e1b
Richard Nguyen <richard.ng0616@gmail.com>
2021-09-26 15:57:24 -0700

    Hot fixed SSG gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,18 +1,40 @@
 /**
  * Gatsby configuration for production
  */
-import React from "react";
+import * as React from "react";
 import { LocationProvider } from "@gatsbyjs/reach-router";
 import "normalize.css";
 
 import GlobalStyle from "./src/components/GlobalStyle";
+import NavbarContext from "./src/components/Navbar/context";
 
+const NavbarProvider = ({ children }) => {
+  const location = useLocation();
+  const [position, setPosition] = React.useState({ top: 76, width: 0, left: 0 });
+
+  const contextValue = React.useMemo(
+    () => ({
+      active: {
+        pathname: location.pathname,
+        top: position.top,
+        left: position.left,
+        width: position.width,
+        onActiveChange: setPosition,
+      },
+    }),
+    [location.pathname, position.top, position.left, position.width]
+  );
+
+  return <NavbarContext.Provider value={contextValue}>{children}</NavbarContext.Provider>;
+};
 // eslint-disable-next-line import/prefer-default-export
 export const wrapPageElement = ({ element }) => {
   return (
     <LocationProvider>
-      <GlobalStyle />
-      {element}
+      <NavbarProvider>
+        <GlobalStyle />
+        {element}
+      </NavbarProvider>
     </LocationProvider>
   );
 };
